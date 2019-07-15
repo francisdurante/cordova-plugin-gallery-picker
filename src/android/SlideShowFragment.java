47e5d394ad571f3661221edff8f64fb755e75eb6
Your Name <you@example.com>
2019-07-15 14:10:44 +0800

    fix

--- src/android/SlideShowFragment.java
+++ src/android/SlideShowFragment.java
@@ -1,12 +1,15 @@
 package cordova.plugin.gallery.picker;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 
 import androidx.annotation.Nullable;
@@ -33,7 +36,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -43,7 +55,7 @@ public class SlideShowFragment extends Fragment {
   static GridView gallery;
   private ArrayList<String> images;
   private ArrayList<String> selectedImage;
-  private String selectedFromCamera;
+  private ArrayList<String> selectedFromCamera;
   private Spinner spinner;
 
   @Nullable
@@ -119,10 +131,17 @@ public class SlideShowFragment extends Fragment {
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     if (requestCode == 1000 && resultCode == RESULT_OK) {
+      selectedFromCamera = new ArrayList<>();
       Bitmap bitmap = (Bitmap)data.getExtras().get("data") ;
-      selectedFromCamera = getRealPathFromURI(getContext(),getImageUri(getContext(),bitmap));
-		ImagePicker.callbackContext.success(selectedFromCamera);
-		getActivity().finish();
+      saveImage(bitmap);
+      selectedFromCamera.add(saveImage(bitmap));
+      JSONArray response = new JSONArray();
+      for(int x = 0 ; x < selectedFromCamera.size(); x++)
+      {
+        response.put(selectedFromCamera.get(x));
+      }
+      ImagePicker.callbackContext.success(response);
+      getActivity().finish();
       //return Callback success;
     }
     else
@@ -152,6 +171,49 @@ public class SlideShowFragment extends Fragment {
     }
   }
 
+  private String saveImage(Bitmap segg) {
+
+    String path = "";
+    OutputStream fOut = null;
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String fileName = "Mystorya-"+ timeStamp +".jpg";
+    String root = Environment.getExternalStorageDirectory().toString();
+    final File imageRoot = new File(root, "Android/data/"+getContext().getPackageName()+"/mystory_images");
+    imageRoot.mkdirs();
+    final File file = new File(imageRoot, fileName);
+    try {
+      fOut = new FileOutputStream(file);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    segg.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+    try {
+      Toast.makeText(getActivity(),
+        file.getAbsolutePath(),
+        Toast.LENGTH_LONG).show();
+      path = file.getAbsolutePath();
+      fOut.flush();
+      fOut.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return path;
+
+//    ContentValues values = new ContentValues();
+//    values.put(MediaStore.Images.Media.TITLE,"stego");
+//    values.put(MediaStore.Images.Media.DESCRIPTION, "stego description");
+//    values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+//    values.put(MediaStore.Images.ImageColumns.BUCKET_ID, file.toString().toLowerCase(Locale.US).hashCode());
+//    values.put(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME, file.getName().toLowerCase(Locale.US));
+//    values.put("_data", file.getAbsolutePath());
+//    Toast.makeText(ExtractActivity.this,
+//      file.getAbsolutePath(),
+//      Toast.LENGTH_LONG).show();
+//    ContentResolver cr = getContext().getContentResolver();
+//    cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//    Toast.makeText(ExtractActivity.this, "The Image thumbnail created in Gallery ", Toast.LENGTH_LONG).show();
+  }
   public void setSelectedIndex(int index, boolean selected) {
     if (selected) {
       selectedImage.add(images.get(index));
